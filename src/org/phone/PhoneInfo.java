3e7a2fe88b501869bf24a8cd211174b8a7dc33b4
krishtvr <krishtvr1995@gmail.com>
2021-10-27 15:12:36 +0530

    tcs step 1

---
+++ src/org/phone/PhoneInfo.java
@@ -0,0 +1,32 @@
+package org.phone;
+
+public class PhoneInfo {
+private void phoneName() {
+	// TODO Auto-generated method stub
+System.out.println("ponename = mi");
+}
+private void phoneMieiNum() {
+	// TODO Auto-generated method stub
+System.out.println("phonemieinum = 0909090");
+}
+private void Camera() {
+	// TODO Auto-generated method stub
+System.out.println("camera = 200mp");
+}
+private void storage() {
+	// TODO Auto-generated method stub
+System.out.println("storage= 64gb");
+}
+private void osName() {
+	// TODO Auto-generated method stub
+System.out.println("osname = java");
+}
+public static void main(String[] args) {
+	PhoneInfo kk = new PhoneInfo();
+	kk.Camera();
+	kk.osName();
+	kk.phoneMieiNum();
+	kk.phoneName();
+	kk.storage();
+}
+}
